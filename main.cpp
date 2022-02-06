b5258ddab182f4b65dcd9f37fe79db090a98054c
Mohaned hassan <Mohanedhassan1999@gmail.com>
2022-02-06 09:30:36 +0000

    added dataset and OpenCV setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
 using namespace std;
 
+
+//build command : g++ --std c++17 -g main.cpp -o output.exe -I C:\OpenCV-MinGW-Build-OpenCV-4.5.2-x64\include -L C:\OpenCV-MinGW-Build-OpenCV-4.5.2-x64\x64\mingw\bin -llibopencv_calib3d452 -llibopencv_core452 -llibopencv_dnn452 -llibopencv_features2d452 -llibopencv_flann452 -llibopencv_highgui452 -llibopencv_imgcodecs452 -llibopencv_imgproc452 -llibopencv_ml452 -llibopencv_objdetect452 -llibopencv_photo452 -llibopencv_stitching452 -llibopencv_video452 -llibopencv_videoio452
 int main(){
+    
+    cout << "hello";
     
 }
